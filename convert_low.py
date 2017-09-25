c266d27150b84b8b8bec7a3e28d96861d2a8b9d3
Miradeljan <miradel51@users.noreply.github.com>
2017-09-25 22:23:49 +0800

    Add files via upload

---
+++ convert_low.py
@@ -0,0 +1,30 @@
+import sys
+import string
+import os
+
+if __name__ == '__main__':
+	in_filename=sys.argv[1]
+	out_file_name=sys.argv[2]
+
+	f_original=open(in_filename,'r')
+	f_lowercase=open(out_file_name,'w')
+
+	#pdb.set_trace()
+	contexts=""
+	lowercase=""
+	#first_prob=""
+	for eachline in f_original:
+		contexts=eachline.strip()
+		#s = 'hEllo pYthon'
+		#print s.upper() #s.lower(),s.capitalize(),s.title()
+		#result HELLO PYTHON,hello python,Hello python,Hello Python
+		lowercase=contexts.lower()
+		f_lowercase.write(lowercase.strip())
+		f_lowercase.write('\n')
+
+f_original.close()
+f_lowercase.close()
+
+
+
+
